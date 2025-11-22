a02bb40112f400321f4e9b57512e004824dba69b
pepperjackdev <giacomodeflorio9093@gmail.com>
2025-11-22 22:21:00 +0100

    Implemented indexing operator on Matrix

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,50 +1,45 @@
-use std::vec;
+use std::{ops::Index, vec};
 
 type Shape = (usize, usize);
 
 pub struct Matrix<T> {
     shape: Shape,
-    data: Vec<Vec<T>>
+    data: Vec<Vec<T>>,
 }
 
 impl<T: Default + Clone> Matrix<T> {
-    pub fn new(shape: Shape) -> Matrix<T> {
+    pub fn new((rows, cols): Shape) -> Matrix<T> {
         Matrix {
-            shape,
-            data: vec![vec![T::default(); shape.1]; shape.0],
+            shape: (rows, cols),
+            data: vec![vec![T::default(); cols]; rows],
         }
     }
 
-    pub fn from<const ROWS: usize, const COLS: usize>(matrix: &[[T; COLS]; ROWS]) -> Matrix<T> {
+    pub fn from<const ROWS: usize, const COLS: usize>(matrix: [[T; COLS]; ROWS]) -> Matrix<T> {
         Matrix {
             shape: (ROWS, COLS),
-            data: matrix.iter()
-                .map(|row| row.to_vec())
-                .collect()
+            data: matrix.iter().map(|row| row.to_vec()).collect(),
         }
     }
 
-    pub fn get_shape<'a>(&'a self) -> &'a Shape {
+    pub fn get_shape(&self) -> &Shape {
         &self.shape
     }
 
-    pub fn get_data<'a>(&'a self) -> &'a Vec<Vec<T>> {
+    pub fn get_data(&self) -> &Vec<Vec<T>> {
         &self.data
+    }
+}
+
+impl<T> Index<usize> for Matrix<T> {
+    type Output = Vec<T>;
+
+    fn index(&self, row: usize) -> &Self::Output {
+        &self.data[row]
     }
 }
 
 #[cfg(test)]
 pub mod tests {
-    use crate::matrix::Matrix;
-
-    #[test]
-    fn test_matrix_from() {
-        let matrix = Matrix::from(&[
-            [1, 2, 3],
-            [4, 5, 6]
-        ]);
-
-        assert_eq!(matrix.get_shape(), &(2, 3));
-        assert_eq!(matrix.get_data(), &vec![vec![1, 2, 3], vec![4, 5, 6]])
-    }
+    
 }
