d345b05f15425f9f3a3fed160fc3d880d5b8991a
pepperjackdev <giacomodeflorio9093@gmail.com>
2025-11-27 22:35:12 +0100

    Added overloading for [] operator

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,12 +1,12 @@
-type Shape = (usize, usize);
+use std::ops::{Index, IndexMut};
 
 pub struct Matrix<T> {
-    shape: Shape,
+    shape: (usize, usize),
     data: Vec<Vec<T>>,
 }
 
 impl<T: Default + Clone> Matrix<T> {
-    pub fn new((rows, cols): Shape) -> Matrix<T> {
+    pub fn new((rows, cols): (usize, usize)) -> Matrix<T> {
         Matrix {
             shape: (rows, cols),
             data: vec![vec![T::default(); cols]; rows],
@@ -20,11 +20,69 @@ impl<T: Default + Clone> Matrix<T> {
         }
     }
 
-    pub fn get_shape(&self) -> &Shape {
+    pub fn get_shape(&self) -> &(usize, usize) {
         &self.shape
     }
+}
 
-    pub fn get_data(&self) -> &Vec<Vec<T>> {
-        &self.data
+impl<T: Default + Clone> Index<(usize, usize)> for Matrix<T> {
+    type Output = T;
+
+    fn index(&self, (row, col): (usize, usize)) -> &Self::Output {
+        &self.data[row][col]
+    }
+}
+
+impl<T: Default + Clone> IndexMut<(usize, usize)> for Matrix<T> {
+    fn index_mut(&mut self, (row, col): (usize, usize)) -> &mut Self::Output {
+        &mut self.data[row][col]
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::matrix::Matrix;
+
+
+    #[test]
+    fn test_matrix_new() {
+        let under_test: Matrix<f32> = Matrix::new((3, 3));
+        assert_eq!(under_test.get_shape(), &(3, 3));
+    }
+
+    #[test]
+    fn test_matrix_from() {
+        let under_test: Matrix<f32> = Matrix::from([
+            [0.0, 1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0, 9.0]
+        ]);
+        
+        assert_eq!(under_test.get_shape(), &(2, 5))
+    }
+
+    #[test]
+    fn test_matrix_index() {
+        let under_test: Matrix<f32> = Matrix::from([
+            [0.0, 1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0, 9.0]
+        ]);
+
+        assert_eq!(under_test[(0, 0)], 0.0);
+        assert_eq!(under_test[(1, 4)], 9.0);
+    }
+
+    #[test]
+    fn test_matrix_index_mut() {
+        let mut under_test: Matrix<f32> = Matrix::from([
+            [0.0, 1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0, 9.0]
+        ]);
+
+        under_test[(0, 0)] = 5.0;
+        under_test[(1, 4)] = 8.0;
+
+        assert_eq!(under_test[(0, 0)], 5.0);
+        assert_eq!(under_test[(1, 4)], 8.0);
     }
 }
