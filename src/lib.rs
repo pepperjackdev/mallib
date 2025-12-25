01c9da8aeea2e10b17a37dfed28526e561878578
pepperjackdev <giacomodeflorio9093@gmail.com>
2025-12-25 19:54:42 +0100

    Declaration of a LinearRegression struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
+use ndarray::{Array1, Array2};
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test() {
-        assert!(true, "Hello, Tests!");
+pub struct LinearRegression {
+    weights: Array1<f64>
+}
+
+impl LinearRegression {
+    pub fn fit(features: &Array2<f64>, targets: &Array1<f64>) -> LinearRegression {
+        todo!["To implement"];
+    }
+
+    pub fn predict(&self, features: &Array2<f64>) -> Array1<f64> {
+        features.dot(&self.weights)
     }
 }
