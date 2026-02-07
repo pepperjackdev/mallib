22fe0443240494a23d99218882dca719a9905ace
pepperjackdev <giacomodeflorio9093@gmail.com>
2026-02-07 18:34:56 +0100

    Matrix transpose and dot product (both naive)

--- include/linear_algebra/matrix.hpp
+++ include/linear_algebra/matrix.hpp
@@ -6,35 +6,73 @@
 
 template <typename T>
 class Matrix {
-private:
-    std::vector<T> data;
-    size_t rows, cols;
+    private:
+        std::vector<T> data;
+        size_t rows, cols;
 
-public:
-    Matrix(size_t rows, size_t cols): 
-        rows(rows), cols(cols), data(rows * cols) {}
+    public:
+        Matrix(size_t rows, size_t cols): 
+            rows(rows), cols(cols), data(rows * cols) {}
 
-    T& operator()(size_t row, size_t col) {
-        return data[cols * row + col];
-    }
+        Matrix(size_t rows, size_t cols, const std::vector<T>& data): 
+            rows(rows), cols(cols), data(data) {
+            if (data.size() != rows * cols) {
+                throw std::invalid_argument(std::format(
+                    "Data does not match the shape ({},{})",
+                    rows, cols
+                ));
+            }
+        }
 
-    const T& operator()(size_t row, size_t col) const {
-        return data[cols * row + col];
-    }
+        T& operator()(size_t row, size_t col) {
+            return data[cols * row + col];
+        }
 
-    std::string toString() const {
-        std::string str = "[";
-        for (size_t row = 0; row < rows; row++) {
-            if (row > 0) str+=" ";
-            str += "[";
-            for (size_t col = 0; col < cols; col++) {
-                str += std::format("{}", (*this)(row, col));
-                if (col + 1 < cols) str += ", ";
+        const T& operator()(size_t row, size_t col) const {
+            return data[cols * row + col];
+        }
+
+        Matrix<T> transpose() const {
+            Matrix<T> transposed(this->cols, this->rows);
+            for (size_t row = 0; row < this->rows; row++) {
+                for (size_t col = 0; col < this->cols; col++) {
+                    transposed(col, row) = (*this)(row, col);
+                }
+            }
+            return transposed;
+        }
+
+        Matrix<T> dot(const Matrix<T>& rhs) const {
+            if (cols != rhs.rows) {
+                throw std::invalid_argument("Matrices are not aligned");
+            }
+
+            Matrix<T> product(this->rows, rhs.cols);
+            for (size_t row = 0; row < product.rows; row++) {
+                for (size_t col = 0; col < product.cols; col++) {
+                    T sum = T{};
+                    for (size_t k = 0; k < this->cols; k++) {
+                        sum += (*this)(row, k) * rhs(k, col);
+                    }
+                    product(row, col) = sum;
+                }
+            }
+            return product;
+        }
+
+        std::string toString() const {
+            std::string str = "[";
+            for (size_t row = 0; row < rows; row++) {
+                if (row > 0) str+=" ";
+                str += "[";
+                for (size_t col = 0; col < cols; col++) {
+                    str += std::format("{}", (*this)(row, col));
+                    if (col + 1 < cols) str += ", ";
+                }
+                str += "]";
+                if (row + 1 < rows) str += ",\n";
             }
             str += "]";
-            if (row + 1 < rows) str += ",\n";
+            return str;
         }
-        str += "]";
-        return str;
-    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 #include "linear_algebra/matrix.hpp"
 
 int main() {
-    Matrix<int> matrix(3, 3);
-    std::cout << matrix.toString() << std::endl;
+    Matrix<int64_t> matrix(3, 3, {
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9
+    });
+
+    Matrix<int64_t> product = matrix.dot(matrix);
+    std::cout << product.toString() << std::endl;
 }
